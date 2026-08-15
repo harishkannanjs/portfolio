d8677689474f662dd659f16ed8c57a216c47bfcf
harishkannanjs <harishkannanjs@gmail.com>
2026-08-15 19:15:54 +0530

    fix(portfolio): update college website link in education section

--- src/features/portfolio/data/education.ts
+++ src/features/portfolio/data/education.ts
@@ -4,7 +4,7 @@ export const EDUCATION: Education[] = [
   {
     id: "grd",
     school: "Dr. G.R. Damodaran College of Science",
-    link: "https://maps.app.goo.gl/K5uAeGZfS9CjbhVm6",
+    link: "https://www.grd.org/grdcs/",
     degree: "B.Sc",
     fieldOfStudy: "Computer Science",
     period: {
